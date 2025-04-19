b1321e29eaead19e0237de1a009188e1a63d6921
Artur Bobotcko <artur.bobotcko@mail.ru>
2025-04-19 04:15:17 +0300

    Init commit

---
+++ archiver.cpp
@@ -0,0 +1,198 @@
+﻿// archiver.cpp: определяет точку входа для приложения.
+//
+
+#include "archiver.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cassert>
+#include <zlib.h>
+#include <string>
+
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
+#endif
+
+using namespace std;
+
+#define CHUNK 16384
+
+int def(FILE* source, FILE* dest, int level)
+{
+    int ret, flush;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate deflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    ret = deflateInit(&strm, level);
+    if (ret != Z_OK)
+        return ret;
+
+    /* compress until end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)deflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+        strm.next_in = in;
+
+        /* run deflate() on input until output buffer not full, finish
+           compression if all of source has been read in */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = deflate(&strm, flush);    /* no bad return value */
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)deflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+        assert(strm.avail_in == 0);     /* all input will be used */
+
+        /* done when last data in file processed */
+    } while (flush != Z_FINISH);
+    assert(ret == Z_STREAM_END);        /* stream will be complete */
+
+    /* clean up and return */
+    (void)deflateEnd(&strm);
+    return Z_OK;
+}
+
+int inf(FILE* source, FILE* dest)
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    ret = inflateInit(&strm);
+    if (ret != Z_OK)
+        return ret;
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR;     /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+}
+
+void zerr(int ret) {
+    fputs("archiver: ", stderr);
+    switch (ret) {
+    case Z_ERRNO:
+        if (ferror(stdin)) {
+            fputs("error reading stdin\n", stderr);
+        }
+        if (ferror(stdout)) {
+            fputs("error writing stdout\n", stderr);
+        }
+        break;
+    case Z_STREAM_ERROR:
+        fputs("invalid compression level\n", stderr);
+        break;
+    case Z_DATA_ERROR:
+        fputs("invalid or incomplete deflate data\n", stderr);
+        break;
+    case Z_MEM_ERROR:
+        fputs("out of memory\n", stderr);
+        break;
+    case Z_VERSION_ERROR:
+        fputs("zlib version mismatch!\n", stderr);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int ret;
+
+    if (argc < 4) {
+        fputs("archiver usage: archiver a <original file> <archive file> - to archive a file\n", stderr);
+        fputs("archiver usage: archiver e <archive file> <original file> - to extract a file", stderr);
+        return 1;
+    }
+    else if (argc == 4) {
+        if (strcmp(argv[1], "a") == 0) {
+            FILE* original_file = fopen(argv[2], "rb");
+            FILE* archive_file = fopen(argv[3], "wb");
+
+            if (original_file == NULL) perror("Error opening original file");
+            if (archive_file == NULL) perror("Error opening archive file");
+
+            int ret = def(original_file, archive_file, Z_DEFAULT_COMPRESSION);
+            if (ret != Z_OK) {
+                zerr(ret);
+            }
+
+            fclose(original_file);
+            fclose(archive_file);
+        }
+        else if (strcmp(argv[1], "e") == 0) {
+            FILE* archive_file = fopen(argv[2], "rb");
+            FILE* original_file = fopen(argv[3], "wb");
+
+            if (original_file == NULL) perror("Error opening original file");
+            if (archive_file == NULL) perror("Error opening archive file");
+
+            int ret = inf(archive_file, original_file);
+            if (ret != Z_OK) {
+                zerr(ret);
+            }
+
+            fclose(archive_file);
+            fclose(original_file);
+        }
+    }
+}
